b2be05b1b69079313497e666b4d5c4b9b16362fd
Oluwatosin Iyiola <tosin.iyiola@cbsystems.ng>
2024-08-24 20:15:33 +0100

    Initial commit

---
+++ src/test/java/com/application/inventarium/InventariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.inventarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
